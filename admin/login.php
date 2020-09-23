5a619a8b9669a4c9fa35ec2680e531fbb240b204
ChhumSeyla <71620947+ChhumSeyla@users.noreply.github.com>
2020-09-23 08:06:37 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -83,7 +83,8 @@ session_start();
   <!-- icheck bootstrap -->
   <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../dist/css/Adminlte.min.css">
+	
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <style type="text/css">
